093bae5b87c7d475baa461d1a67a34eafbacf2c3
Chandan Pathak <vijaypathakssa3@gmail.com>
2026-05-12 19:34:01 +0530

    feat: wrap app with page transitions and global ux components

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,50 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AdminPanel from './components/AdminPanel';
+import ScrollProgress from './components/ScrollProgress';
+import BackToTop from './components/BackToTop';
+import CursorGlow from './components/CursorGlow';
+import PageTransition from './components/PageTransition';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import SkillsPage from './pages/SkillsPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
+import NotFoundPage from './pages/NotFoundPage';
 import { getProjects, getSkills } from './utils/dataManager';
 
 function Layout() {
+    const location = useLocation();
+
     // Prefetch data immediately on mount so Skills & Projects sections
     // render instantly once the user scrolls to them.
     useEffect(() => {
         getProjects();
         getSkills();
     }, []);
+
     return (
         <div className="min-h-screen flex flex-col">
+            <ScrollProgress />
+            <CursorGlow />
             <Navbar />
             <div className="flex-1">
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/skills" element={<SkillsPage />} />
-                    <Route path="/projects" element={<ProjectsPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
+                <PageTransition key={location.pathname}>
+                    <Routes location={location}>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<AboutPage />} />
+                        <Route path="/skills" element={<SkillsPage />} />
+                        <Route path="/projects" element={<ProjectsPage />} />
+                        <Route path="/contact" element={<ContactPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </PageTransition>
             </div>
             <Footer />
+            <BackToTop />
         </div>
     );
 }
